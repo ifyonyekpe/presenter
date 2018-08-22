8fdf12762b74000e67bc420d378d37ea7b6cc9ee
ionyekpe <ionyekpe@goempyrean.com>
2018-08-22 16:22:17 -0500

    modified startup

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,45 @@
-import { inject } from 'aurelia-framework'
-import Remarkable from 'remarkable'
-import { SlideContents } from './slideContent/farmers/index'
-import { map, curry } from 'ramda'
+import { inject } from "aurelia-framework";
+import Remarkable from "remarkable";
+import { SlideContents } from "./slideContent/aimPremium/index";
+import { map, curry } from "ramda";
 
 @inject(Remarkable)
 export class App {
-  markdown: Remarkable
-  slides: { text: string }[]
-  currentIndex: number = 0
-  constructor(markdown: Remarkable) {
-    this.markdown = markdown
-  }
+  slides: { text: string }[];
+  currentIndex: number = 0;
+  constructor(private markdown: Remarkable) {}
 
   attached() {
     this.slides = map(c => {
       return {
-        text: this.markdown.render(c.text),
-      }
-    }, SlideContents)
+        text: this.markdown.render(c.text)
+      };
+    }, SlideContents);
 
-    document.addEventListener('keydown', this.keydownInput(this), false)
+    document.addEventListener("keydown", this.keydownInput(this), false);
   }
 
   detached() {
-    document.removeEventListener('keydown', this.keydownInput(this))
+    document.removeEventListener("keydown", this.keydownInput(this));
   }
 
   previous() {
     if (this.currentIndex > 0) {
-      this.currentIndex--
+      this.currentIndex--;
     }
   }
 
   next() {
     if (this.currentIndex < this.slides.length - 1) {
-      this.currentIndex++
+      this.currentIndex++;
     }
   }
 
   keydownInput = curry((c, event) => {
     // Right
-    if (event.keyCode == 39) c.next()
+    if (event.keyCode == 39) c.next();
 
     // Left
-    if (event.keyCode == 37) c.previous()
-  })
+    if (event.keyCode == 37) c.previous();
+  });
 }
